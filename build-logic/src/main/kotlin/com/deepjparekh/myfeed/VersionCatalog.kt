d5de5e6aba8cd8051048a54cdd3e28ab6abfa94a
Deep Parekh <deepjparekh@gmail.com>
2026-04-05 10:14:12 -0400

    fix: use toIntOrNull with error message for safer version parsing

--- build-logic/src/main/kotlin/com/deepjparekh/myfeed/VersionCatalog.kt
+++ build-logic/src/main/kotlin/com/deepjparekh/myfeed/VersionCatalog.kt
@@ -4,5 +4,6 @@ import org.gradle.api.artifacts.VersionCatalog
 
 internal fun VersionCatalog.versionInt(alias: String): Int {
     val version = findVersion(alias).get().requiredVersion
-    return version.toInt()
+    return version.toIntOrNull()
+        ?: error("Version for alias '$alias' must be an integer, but was '$version'")
 }
